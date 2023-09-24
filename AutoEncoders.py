643b2047c1b0d66bcdf3cfa7663b3884122b51c7
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-09-24 10:37:38 +0200

    Completion of auto encoders

--- AutoEncoders.py
+++ AutoEncoders.py
@@ -1,80 +1,102 @@
 import numpy as np
 import tensorflow as tf
-from keras.datasets import cifar10
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Flatten, Conv2D, MaxPooling2D, UpSampling2D
-from keras.utils import to_categorical
-from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, roc_auc_score, roc_curve
-from sklearn.metrics import precision_score, recall_score, f1_score, cohen_kappa_score
-import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.layers import Input, Dense, Flatten, Reshape
+from tensorflow.keras.models import Model
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    confusion_matrix,
+    roc_auc_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    cohen_kappa_score,
+)
+from sklearn.preprocessing import LabelBinarizer
 
-# Load CIFAR-10 dataset
+# Load the CIFAR-10 dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-# Preprocess data
-x_train = x_train.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+# Normalize the data
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
-# Define and train the autoencoder
+# Autoencoder architecture
 input_layer = Input(shape=(32, 32, 3))
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_layer)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+flatten_layer = Flatten()(input_layer)
+encoder_output = Dense(128, activation="relu")(flatten_layer)
+decoder_output = Dense(32 * 32 * 3, activation="sigmoid")(encoder_output)
+reshape_layer = Reshape((32, 32, 3))(decoder_output)
 
-x = Conv2D(128, (3, 3), activation='relu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)
+autoencoder = Model(inputs=input_layer, outputs=reshape_layer)
 
-autoencoder = Model(input_layer, decoded)
-autoencoder.compile(optimizer='adam', loss='mse')
+# Encoder model for feature extraction
+encoder = Model(inputs=input_layer, outputs=encoder_output)
 
-autoencoder.fit(x_train, x_train, epochs=20, batch_size=128, validation_data=(x_test, x_test))
+# Compile the autoencoder
+autoencoder.compile(optimizer="adam", loss="mean_squared_error")
 
-# Create the classifier model by using the encoder part of the autoencoder
-classifier_input = Input(shape=(32, 32, 3))
-classifier_layer = autoencoder.layers[1](classifier_input)
-classifier_layer = autoencoder.layers[2](classifier_layer)
-classifier_layer = autoencoder.layers[3](classifier_layer)
-classifier_layer = autoencoder.layers[4](classifier_layer)
-classifier_layer = Flatten()(classifier_layer)
-classifier_layer = Dense(128, activation='relu')(classifier_layer)
-classifier_layer = Dense(64, activation='relu')(classifier_layer)
-classifier_output = Dense(10, activation='softmax')(classifier_layer)
+# Train the autoencoder
+autoencoder.fit(
+    x_train,
+    x_train,
+    epochs=10,
+    batch_size=256,
+    shuffle=True,
+    validation_data=(x_test, x_test),
+)
 
-classifier = Model(classifier_input, classifier_output)
+# Extract features using the encoder
+x_train_encoded = encoder.predict(x_train)
+x_test_encoded = encoder.predict(x_test)
 
-# Compile and train the classifier
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-classifier.fit(x_train, y_train, epochs=20, batch_size=128, validation_data=(x_test, y_test))
+# Convert labels to one-hot encoded format
+lb = LabelBinarizer()
+y_train_encoded = lb.fit_transform(y_train)
+y_test_encoded = lb.transform(y_test)
 
-# Evaluate the classifier
-y_pred = classifier.predict(x_test)
-y_pred_classes = np.argmax(y_pred, axis=1)
-y_true = np.argmax(y_test, axis=1)
+# Define a classifier (you can use any classifier of your choice)
+classifier = tf.keras.Sequential(
+    [
+        Dense(128, activation="relu", input_shape=(128,)),
+        Dense(64, activation="relu"),
+        Dense(10, activation="softmax"),
+    ]
+)
 
-# Performance metrics
-accuracy = accuracy_score(y_true, y_pred_classes)
-confusion = confusion_matrix(y_true, y_pred_classes)
-roc_auc = roc_auc_score(y_true, y_pred, multi_class='ovr')
-precision = precision_score(y_true, y_pred_classes, average='weighted')
-recall = recall_score(y_true, y_pred_classes, average='weighted')
-f1 = f1_score(y_true, y_pred_classes, average='weighted')
-kappa = cohen_kappa_score(y_true, y_pred_classes)
+classifier.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-# Print results
-print(f'Accuracy: {accuracy}')
-print(f'Confusion Matrix:\n{confusion}')
-print(f'AUC/ROC: {roc_auc}')
-print(f'Precision: {precision}')
-print(f'Recall: {recall}')
-print(f'F1 Score: {f1}')
-print(f'Kappa: {kappa}')
+# Train the classifier on the encoded features
+classifier.fit(
+    x_train_encoded,
+    y_train_encoded,
+    epochs=10,
+    batch_size=256,
+    shuffle=True,
+    validation_data=(x_test_encoded, y_test_encoded),
+)
+
+# Predictions
+y_pred = classifier.predict(x_test_encoded)
+y_pred_labels = np.argmax(y_pred, axis=1)
+y_true_labels = np.argmax(y_test_encoded, axis=1)
+
+# Calculate performance metrics
+accuracy = accuracy_score(y_true_labels, y_pred_labels)
+conf_matrix = confusion_matrix(y_true_labels, y_pred_labels)
+roc_auc = roc_auc_score(y_test_encoded, y_pred)
+precision = precision_score(y_true_labels, y_pred_labels, average="macro")
+recall = recall_score(y_true_labels, y_pred_labels, average="macro")
+f1 = f1_score(y_true_labels, y_pred_labels, average="macro")
+kappa = cohen_kappa_score(y_true_labels, y_pred_labels)
+
+# Display performance metrics
+print("Accuracy:", accuracy)
+print("Confusion Matrix:")
+print(conf_matrix)
+print("ROC AUC:", roc_auc)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
+print("Kappa:", kappa)
